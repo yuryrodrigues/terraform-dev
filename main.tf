fe85de7cc245e9e008d7fb546187be9a4d5c47bc
Yury Rodrigues <yuryrodrigues28@gmail.com>
2022-06-02 22:09:42 -0300

    starting

---
+++ main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization  = "yury-dev"
+    workspaces {
+      name  = "terraform-lab"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "eastus2"
+  
+  tags  = {
+      Environment = "TF"
+      Team        = "Dev"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet-v2"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
